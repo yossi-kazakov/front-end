0728fa21343dc1512d626849793211e785951c08
yossi-kazakov <yossi.kazakov@allbetter.co.il>
2023-12-22 09:15:26 +0200

    ddas

--- src/components/CreatePostCard.js
+++ src/components/CreatePostCard.js
@@ -4,10 +4,9 @@ import { useState } from 'react'
 import { Form } from 'react-bootstrap';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 
-const CreatePostCard = ({ mockData, day }) => {
+const CreatePostCard = ({ mockData, day, showCreatePostModal, setShowCreatePostModal }) => {
     const [newPostData, setNewPostData] = useState({});
-    
-
+    const [successVisible, setSuccessVisible] = useState(false);
     const handleInputChange = (e) => {
         const key = e.target.name;
         const value = e.target.value;
@@ -18,33 +17,35 @@ const CreatePostCard = ({ mockData, day }) => {
         const newPost = {
             id: mockData.length + 1,
             date: `${day}.10`,
+            author: newPostData.author || 'אנונימי',
             title: newPostData.title,
             content: newPostData.content,
         }
+        console.log(newPost);
         mockData.push(newPost);
         setShowCreatePostModal(false);
+        setSuccessVisible(true); 
     }
 
     return (
         <div className='create-post'>
-
             {showCreatePostModal ? <Form >
                 <Form.Group className='mb-3'>
                     <Form.Label>מה שמך? (שדה זה אינו חובה)</Form.Label>
-                    <Form.Control type="text" placeholder="" onChange={handleInputChange} />
+                    <Form.Control type="text" name="author" onChange={handleInputChange} />
                 </Form.Group>
                 <Form.Group className='mb-3'>
                     <Form.Label>מהי הכותרת של השיתוף שלך?</Form.Label>
-                    <Form.Control type="text" placeholder="" required onChange={handleInputChange} />
+                    <Form.Control type="text" name="title" required onChange={handleInputChange} />
                 </Form.Group>
                 <Form.Group className='mb-3'>
                     <Form.Label>תוכן השיתוף</Form.Label>
-                    <Form.Control as="textarea" rows={3} required onChange={handleInputChange} />
+                    <Form.Control as="textarea" rows={3} name="content" required onChange={handleInputChange} />
                 </Form.Group>
                 <button className="btn btn-primary" onClick={handlePostSubmit}>שתף</button>
             </Form>
                 :
-                <div className="success">
+                <div className={`success ${successVisible ? 'visible' : ''}`}>
                     <CheckCircleOutlineIcon style={{ fontSize: 100, color: 'green' }} />
                     <h1>תודה!</h1>
                     <h2>השיתוף עלה בהצלחה</h2>
